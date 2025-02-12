fb60f632a76f3707c6de785e8cd4c82fef616aa2
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-02-12 23:17:41 +0100

    Funciona boton abrir ajustes

--- app/src/main/java/com/jbc/androidlauncher/MainActivity.kt
+++ app/src/main/java/com/jbc/androidlauncher/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jbc.androidlauncher
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,37 +13,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jbc.androidlauncher.presentation.LauncherScreen
 import com.jbc.androidlauncher.ui.theme.AndroidLauncherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            AndroidLauncherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+        fun abrirApp(packageName: String) {
+            val pm = packageManager
+            try {
+                pm.getPackageInfo(packageName, 0)
+                val intent = pm.getLaunchIntentForPackage(packageName)
+                if (intent != null) {
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "No se pudo abrir la aplicación", Toast.LENGTH_SHORT).show()
                 }
+            } catch (e: PackageManager.NameNotFoundException) {
+                Toast.makeText(this, "La aplicación no está instalada", Toast.LENGTH_SHORT).show()
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidLauncherTheme {
-        Greeting("Android")
+        setContent {
+            LauncherScreen { abrirApp("com.android.settings") }
+        }
     }
 }
