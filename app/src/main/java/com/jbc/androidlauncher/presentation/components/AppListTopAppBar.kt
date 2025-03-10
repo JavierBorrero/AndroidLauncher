8766f7bdc66d424678fb12e49d8f38b96a181404
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-03-10 12:56:35 +0100

    top app bar en archivo separado + animacion textfield

---
+++ app/src/main/java/com/jbc/androidlauncher/presentation/components/AppListTopAppBar.kt
@@ -0,0 +1,103 @@
+package com.jbc.androidlauncher.presentation.components
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jbc.androidlauncher.R
+import com.jbc.androidlauncher.ui.theme.BackgroundGrey
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
+@Composable
+fun AppListTopAppBar(
+    isSearching: Boolean,
+    searchText: String,
+    onSearchTextChanged: (String) -> Unit,
+    onToggleSearch: (Boolean) -> Unit,
+) {
+    TopAppBar(
+        modifier = Modifier
+            .padding(top = 18.dp),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = BackgroundGrey,
+        ),
+        title = {
+
+            AnimatedContent(
+                targetState = isSearching,
+                content = { isSearching ->
+                    if (isSearching) {
+                        TextField(
+                            value = searchText,
+                            onValueChange = onSearchTextChanged,
+                            placeholder = {
+                                Text(
+                                    text = "Buscar...",
+                                    fontFamily = FontFamily.Monospace,
+                                    fontSize = 20.sp,
+                                )
+                            },
+                            singleLine = true,
+                            modifier = Modifier.fillMaxWidth(),
+                            textStyle = TextStyle(
+                                color = Color.White,
+                                fontSize = 20.sp,
+                                fontFamily = FontFamily.Monospace
+                            ),
+                            colors = TextFieldDefaults.colors(
+                                focusedContainerColor = BackgroundGrey,
+                                unfocusedContainerColor = BackgroundGrey,
+                                cursorColor = Color.White,
+                            ),
+                        )
+                    } else {
+                        Text(
+                            fontSize = 30.sp,
+                            text = "Aplicaciones",
+                            color = Color.White,
+                            fontFamily = FontFamily.Monospace,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                },
+                transitionSpec = {
+                    slideInHorizontally(initialOffsetX = { 0 }) togetherWith slideOutHorizontally(targetOffsetX = { 0 })
+                }
+            )
+        },
+        actions = {
+            IconButton(onClick = { onToggleSearch(!isSearching) }) {
+                Image(
+                    painter = painterResource(
+                        if (isSearching) R.drawable.icon_arrow_back else R.drawable.icon_search
+                    ),
+                    contentDescription = "icon",
+                    modifier = Modifier.size(34.dp),
+                    colorFilter = ColorFilter.tint(Color.White)
+                )
+            }
+        }
+    )
+}
--- app/src/main/java/com/jbc/androidlauncher/presentation/screens/applist/AppListScreen.kt
+++ app/src/main/java/com/jbc/androidlauncher/presentation/screens/applist/AppListScreen.kt
@@ -16,32 +16,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
-import com.jbc.androidlauncher.R
 import com.jbc.androidlauncher.data.AppInfo
 import com.jbc.androidlauncher.presentation.components.AppDialog
+import com.jbc.androidlauncher.presentation.components.AppListTopAppBar
 import com.jbc.androidlauncher.ui.theme.BackgroundGrey
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -74,56 +65,11 @@ fun AppListScreen(appListViewModel: AppListViewModel) {
             .background(BackgroundGrey)
     ) {
 
-        TopAppBar(
-            modifier = Modifier
-                .padding(top = 18.dp),
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = BackgroundGrey,
-            ),
-            title = {
-                if(isSearching) {
-                    TextField(
-                        value = searchText,
-                        onValueChange = appListViewModel::onSearchTextChanged,
-                        placeholder = { Text("Buscar...") },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth(),
-                        textStyle = TextStyle(color = Color.White),
-                        colors = TextFieldDefaults.colors(
-                            unfocusedContainerColor = BackgroundGrey,
-                            cursorColor = Color.White
-                        )
-                    )
-                } else {
-                    Text(
-                        fontSize = 30.sp,
-                        text = "Aplicaciones",
-                        color = Color.White,
-                        fontFamily = FontFamily.Monospace,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-            },
-            actions = {
-
-                if(isSearching) {
-                    IconButton(onClick = { appListViewModel.setSearch(false) }) {
-                        Image(
-                            painter = painterResource(R.drawable.icon_arrow_back),
-                            contentDescription = "icon",
-                            modifier = Modifier.size(34.dp)
-                        )
-                    }
-                } else {
-                    IconButton(onClick = { appListViewModel.setSearch(true) }) {
-                        Image(
-                            painter = painterResource(R.drawable.icon_search),
-                            contentDescription = "icon",
-                            modifier = Modifier.size(34.dp)
-                        )
-                    }
-                }
-            }
+        AppListTopAppBar(
+            isSearching,
+            searchText,
+            onSearchTextChanged = appListViewModel::onSearchTextChanged,
+            onToggleSearch = appListViewModel::setSearch
         )
 
         LazyColumn(
