17f86528d3f8166e5ef5008883c44294f8ff08fd
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-03-12 17:30:34 +0100

    mejor boton para pasar a AppList

--- app/src/main/java/com/jbc/androidlauncher/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/jbc/androidlauncher/presentation/screens/main/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.jbc.androidlauncher.presentation.screens.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,19 +10,28 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.jbc.androidlauncher.R
 import com.jbc.androidlauncher.presentation.screens.applist.AppItem
 import com.jbc.androidlauncher.ui.theme.BackgroundGrey
 import kotlinx.coroutines.delay
@@ -52,15 +62,6 @@ fun MainScreen(
     Column(
         modifier = Modifier.fillMaxSize()
     ){
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(BackgroundGrey)
-                .padding(start = 10.dp, top = 36.dp),
-            onClick = { navigateToList() }
-        ) {
-            Text("Ir a listado apps")
-        }
 
         SystemInfo(
             batteryLevel,
@@ -69,12 +70,39 @@ fun MainScreen(
             onCalendarClick = { context.startActivity(calendarIntent) }
         )
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(BackgroundGrey)
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 10.dp),
+                text = "Tus Aplicaciones",
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+
+            IconButton(onClick = navigateToList) {
+                Image(
+                    painter = painterResource(R.drawable.icon_add),
+                    contentDescription = "icon",
+                    modifier = Modifier.size(30.dp),
+                    colorFilter = ColorFilter.tint(Color.White)
+                )
+            }
+        }
+
         LazyColumn(
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
                 .background(BackgroundGrey)
-                .padding(top = 10.dp)
         ) {
             items(apps) { app ->
                 AppItem(
@@ -97,7 +125,8 @@ fun SystemInfo(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(BackgroundGrey),
+            .background(BackgroundGrey)
+            .padding(top = 36.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
